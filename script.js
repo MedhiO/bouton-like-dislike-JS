b3d1c99df75bc192d95423f4e856e64c7a611143
Medhi OUKILI <medhioukili@outlook.fr>
2023-10-19 09:47:00 +0200

    script

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Sélectionnez le bouton par son ID
+// Sélectionne le bouton par son ID
 const boutonLike = document.getElementById("like");
 
-// Initialisez une variable d'état
+// Initialise une variable d'état
 let etat = "j-aime"; // "j-aime" par défaut
 
-// Ajoutez un gestionnaire d'événements pour le clic sur le bouton
+// Ajoute un gestionnaire d'événements pour le clic sur le bouton
 boutonLike.addEventListener("click", function() {
   if (etat === "j-aime") {
-    boutonLike.textContent = "Je n'aime pas"; // Changez le texte
-    etat = "je-naime-pas"; // Changez l'état
+    boutonLike.textContent = "Je n'aime pas"; // Change le texte
+    etat = "je-naime-pas"; // Change l'état
   } else {
-    boutonLike.textContent = "J'aime"; // Changez le texte de retour
-    etat = "j-aime"; // Revenez à l'état initial
+    boutonLike.textContent = "J'aime"; // Change le texte de retour
+    etat = "j-aime"; // Reviens à l'état initial
   }
 });
